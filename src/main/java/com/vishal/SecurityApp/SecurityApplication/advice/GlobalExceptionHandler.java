0ed9cf1da5aa254ec25fbcfa76e99367a0f41fc9
vishal patidar <vishalsuman8770@gmail.com>
2025-12-25 21:35:01 +0530

    implement global exception handling for ResourceNotFoundException

---
+++ src/main/java/com/vishal/SecurityApp/SecurityApplication/advice/GlobalExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.vishal.SecurityApp.SecurityApplication.advice;
+
+
+import com.vishal.SecurityApp.SecurityApplication.exceptions.ResourceNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ApiError> handleResourceNotFoundException(ResourceNotFoundException exception) {
+        ApiError apiError = new ApiError(exception.getLocalizedMessage(), HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(apiError, HttpStatus.NOT_FOUND);
+    }
+}
