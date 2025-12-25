a94eb6ccc323e486ff5681dcfef484ae6f4481e6
vishal patidar <vishalsuman8770@gmail.com>
2025-12-25 21:38:51 +0530

    add custom ResourceNotFoundException

---
+++ src/main/java/com/vishal/SecurityApp/SecurityApplication/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.vishal.SecurityApp.SecurityApplication.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
