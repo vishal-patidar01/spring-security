71895d9ca5cc0424c75b36cae2a72080e48dd64c
vishal patidar <vishalsuman8770@gmail.com>
2025-12-25 21:37:08 +0530

    Provided ModelMapper bean for DTO–entity conversion

---
+++ src/main/java/com/vishal/SecurityApp/SecurityApplication/config/AppConfig.java
@@ -0,0 +1,14 @@
+package com.vishal.SecurityApp.SecurityApplication.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AppConfig {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+}
