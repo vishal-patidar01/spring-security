e7dcbcc129d486f58c0752413099d580817267af
vishal patidar <vishalsuman8770@gmail.com>
2025-12-26 12:24:43 +0530

    implement PostServiceImpl with CRUD operations and ModelMapper integration

---
+++ src/main/java/com/vishal/SecurityApp/SecurityApplication/services/PostServiceImpl.java
@@ -0,0 +1,43 @@
+package com.vishal.SecurityApp.SecurityApplication.services;
+
+import com.vishal.SecurityApp.SecurityApplication.dto.PostDto;
+import com.vishal.SecurityApp.SecurityApplication.entities.PostEntity;
+import com.vishal.SecurityApp.SecurityApplication.exceptions.ResourceNotFoundException;
+import com.vishal.SecurityApp.SecurityApplication.repositories.PostRepository;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class PostServiceImpl implements PostService{
+
+    private final PostRepository postRepository;
+    private final ModelMapper modelMapper;
+
+    @Override
+    public List<PostDto> getAllPost() {
+        return postRepository
+                .findAll()
+                .stream()
+                .map(postEntity -> modelMapper.map(postEntity, PostDto.class))
+                .toList();
+    }
+
+    @Override
+    public PostDto getPostById(Long postId) {
+        PostEntity postEntity = postRepository
+                .findById(postId)
+                .orElseThrow(() -> new ResourceNotFoundException("Post not found with id: "+ postId));
+
+        return modelMapper.map(postEntity, PostDto.class);
+    }
+
+    @Override
+    public PostDto createNewPost(PostDto inputPost) {
+        PostEntity postEntity = modelMapper.map(inputPost, PostEntity.class);
+        return modelMapper.map(postRepository.save(postEntity), PostDto.class);
+    }
+}
