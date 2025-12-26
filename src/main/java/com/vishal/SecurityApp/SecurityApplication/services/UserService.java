c5276aa32c45c461751df48a04e5644648fa62e3
vishal patidar <vishalsuman8770@gmail.com>
2025-12-26 12:25:34 +0530

    implement UserService with UserDetailsService for loading users by email

---
+++ src/main/java/com/vishal/SecurityApp/SecurityApplication/services/UserService.java
@@ -0,0 +1,22 @@
+package com.vishal.SecurityApp.SecurityApplication.services;
+
+import com.vishal.SecurityApp.SecurityApplication.exceptions.ResourceNotFoundException;
+import com.vishal.SecurityApp.SecurityApplication.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService implements UserDetailsService{
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username)
+                .orElseThrow(() -> new ResourceNotFoundException("user with email "+ username +"not found"));
+    }
+}
