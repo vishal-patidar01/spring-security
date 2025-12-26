ff34e21203f475af8bcdc55b74e0aee0d17a78c6
vishal patidar <vishalsuman8770@gmail.com>
2025-12-26 16:48:03 +0530

    Implement Spring Security configuration with in-memory users and role-based access control

---
+++ src/main/java/com/vishal/SecurityApp/SecurityApplication/config/WebSecurityConfig.java
@@ -0,0 +1,57 @@
+package com.vishal.SecurityApp.SecurityApplication.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) {
+
+        httpSecurity
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/posts").permitAll()
+                        .requestMatchers("/posts/**").hasAnyRole("ADMIN")
+                        .anyRequest().authenticated())
+                .csrf(config -> config.disable())
+                .sessionManagement(sessionConfig -> sessionConfig
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .formLogin(Customizer.withDefaults());
+        return httpSecurity.build();
+    }
+
+    @Bean
+    UserDetailsService myInMemoryUserDetailsService() {
+        UserDetails normalUser = User
+                .withUsername("vishal")
+                .password(passwordEncoder().encode("vishal123"))
+                .roles("USER")
+                .build();
+
+        UserDetails adminUser = User
+                .withUsername("admin")
+                .password(passwordEncoder().encode("admin"))
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(normalUser, adminUser);
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
