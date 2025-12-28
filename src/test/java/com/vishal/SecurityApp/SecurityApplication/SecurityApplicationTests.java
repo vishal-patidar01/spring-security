b8df4e9dade2ae11eaaa81e790dc4bae426ba8ee
vishal patidar <vishalsuman8770@gmail.com>
2025-12-28 18:36:42 +0530

    test(jwt): add contextLoads test for token generation and userId extraction

--- src/test/java/com/vishal/SecurityApp/SecurityApplication/SecurityApplicationTests.java
+++ src/test/java/com/vishal/SecurityApp/SecurityApplication/SecurityApplicationTests.java
@@ -15,7 +15,7 @@ class SecurityApplicationTests {
 	@Test
 	void contextLoads() {
 
-        User user = new User(4L, "vishal@gmail.com", "1234");
+        User user = new User(4L, "vishal@gmail.com", "1234", "vishal");
 
         String token = jwtService.generateToken(user);
 
