63979c2948dacae6b74402a4e8b00a0b10dae5a4
vishal patidar <vishalsuman8770@gmail.com>
2025-12-28 18:28:48 +0530

    Add ToString formate

--- src/main/java/com/vishal/SecurityApp/SecurityApplication/entities/User.java
+++ src/main/java/com/vishal/SecurityApp/SecurityApplication/entities/User.java
@@ -1,10 +1,7 @@
 package com.vishal.SecurityApp.SecurityApplication.entities;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.jspecify.annotations.Nullable;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -17,6 +14,7 @@ import java.util.List;
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
+@ToString
 public class User implements UserDetails {
 
     @Id
